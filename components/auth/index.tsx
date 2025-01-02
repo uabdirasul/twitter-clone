31eb6abc98197deee57ae7bc924f31cdaae9d25e
uabdirasul <ubbiniyazov04@gmail.com>
2025-01-02 11:19:42 +0500

    add login and register modals

--- components/auth/index.tsx
+++ components/auth/index.tsx
@@ -1,11 +1,31 @@
+"use client";
+
+import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegisterModal";
 import Image from "next/image";
+import { useCallback } from "react";
 import { AiFillGithub as GithubIcon } from "react-icons/ai";
 import { FcGoogle as GoogleIcon } from "react-icons/fc";
+import LoginModal from "../modals/login-modal";
+import RegisterModal from "../modals/register-modal";
 import Button from "../ui/button";
 
 export default function Auth() {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+
+  const onOpenRegisterModal = useCallback(() => {
+    registerModal.onOpen();
+  }, [registerModal]);
+
+  const onOpenLoginModal = useCallback(() => {
+    loginModal.onOpen();
+  }, [loginModal]);
+
   return (
     <div className="container h-screen mx-auto max-w-7xl px-4">
+      <RegisterModal />
+      <LoginModal />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center h-screen">
         <Image
           src={"/images/x.svg"}
@@ -48,14 +68,23 @@ export default function Auth() {
                 <p className="mx-4">or</p>
                 <div className="h-px bg-gray-700 w-1/2" />
               </div>
-              <Button label="Create Account" fullWidth></Button>
+              <Button
+                label="Create Account"
+                fullWidth
+                onClick={onOpenRegisterModal}
+              ></Button>
             </div>
           </div>
           <div className="w-full md:w-[60%]">
             <h3 className="font-medium text-xl mb-4">
               Already have an account?
             </h3>
-            <Button label={"Sign in"} fullWidth outline></Button>
+            <Button
+              label={"Sign in"}
+              fullWidth
+              outline
+              onClick={onOpenLoginModal}
+            ></Button>
           </div>
         </div>
       </div>
